671a89b2a45a9a6b80d308a21a33a5a6c031dfe0
nglu22 <37162714+nglu22@users.noreply.github.com>
2018-04-16 14:52:13 +1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+import BusinessList from './BusinessList';
+import SearchBar from './SearchBar';
+
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+
+}
+const businesses=[business, business, business, business, business, business];
+
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>famished</h1>
+        <SearchBar/>
+        <BusinessList businesses={businesses}/>
+      </div>
+
+    );
+  }
+
+}
+
+export default App;
